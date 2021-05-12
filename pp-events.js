8f19a6df2683661957753b3dc6ca832aa4723244
carlos-sweb <c4rl0sill3sc4@gmail.com>
2021-05-11 20:04:53 -0400

    update

--- pp-events.js
+++ pp-events.js
@@ -1,86 +1,91 @@
 /*!!
  * Power Panel Events <https://github.com/carlos-sweb/pp-events>
  * @author Carlos Illesca
- * @version 1.0.5 (2020/03/24 22:27 PM)
+ * @version 1.0.6 (2020/05/11 19:51 PM)
  * Released under the MIT License
  */
-(function(global , factory ){
-  	
-  	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  	
-  	typeof define === 'function' && define.amd ? define('ppEvents', factory) :
-	
-	(global = global || self, (function () {
-        
-    var exports = global.ppEvents = factory();    
-
+(function(global,factory){
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define('ppEvents', factory) :
+  (global = global || self, (function () {
+    var exports = global.ppEvents = factory();
 	}()
-	
 ));
-
-})( this,(function() {
-	return function(){
-			/**
-			*@property {object} events - Container of events
-			*@author Carlos Illesca <c4rl0sill3sc4@gmail.com>
-			*@memberof ppEvents
-			*/
-			this.events = {};
-			/**
-			*on
-			*@param {string} eventName - name event 
-			*@param {function} callbacks - Function for execute when emit event name
-			*@author Carlos Illesca <c4rl0sill3sc4@gmail.com>
-			*@memberof ppEvents
-			*@returns {void} 
-			*/
-			this.on = function( eventName , callbacks ){
-				if( typeof eventName == "string" ){
-					if( typeof callbacks == "function" ){            
-						if( !this.events.hasOwnProperty(eventName) ){
-							this.events[ eventName ] = [];
-						}            
-					this.events[ eventName ].push( callbacks );
-					}
-				}  
-			}
-			/**
-			*emit
-			*@param {string} eventName - name for events call
-			*@memberof ppEvents
-			*@returns {void}	    
-			*/
-			this.emit = function( eventName ){        
-				var i, listeners, length, args = [].slice.call(arguments, 1);
-				if (typeof this.events[eventName] === 'object') {
-				  listeners = this.events[eventName].slice();         
-				  length = listeners.length;
-				  for (i = 0; i < length; i++) {
-				      listeners[i].apply(this, args);
-				  }
-				}
-			}	  
-			/**
-			*emit
-			*@param {string} eventName - name for events call
-			*@param {funcion} function - funcion will be remove 
-			*@memberof ppEvents
-			*@returns {void}	    
-			*/
-			this.removeListener = function( eventName , callbacks ){
-				var idx; 
-				if( typeof this.events[eventName] === 'object' ){
-					
-					idx = this.events[eventName].indexOf(callbacks);
-										
-					if( idx > -1 ){
-
-						this.events[eventName].splice(idx,1);
-
-					}
-					
-				}
-			}			
-//---------------------------------------
+})(this,function(){
+	/**
+	*@var {object} events - Container of events	
+	*/	
+  	var events = {};
+  	/*
+  	*@var {function} toString - link of native function from object prototype	
+  	*/
+  	var toString =  Object.prototype.toString;
+  	/*
+  	*@var {function} isString - check if value is type String
+  	*/
+	var isString = function( value  ){
+	 return toString.call( value ) === '[object String]'
 	}
-}));	
+	/*
+  	*@var {function} isFunction - check if value is type String
+  	*/
+	var isFunction = function( value  ){
+	 return toString.call( value ) === '[object Function]'
+	}
+	/*
+  	*@var {function} ppEvents - main function
+  	*/
+  	var ppEvents = function(){},
+	/*
+	*@var {object} proto - link to prototype from main function
+	*/
+  	proto = ppEvents.prototype;
+	/**
+	*on
+	*@param {string} eventName - name event 
+	*@param {function} callbacks - Function for execute when emit event name
+	*@returns {void} 
+	*/
+	proto.on = function( eventName , callbacks ){
+	  if( isString( eventName ) && isFunction(callbacks) ){
+	    if( !events.hasOwnProperty(eventName) ){
+	      events[ eventName ] = []
+	    }
+	    events[ eventName ].push( callbacks );
+	  }
+	}
+	/**
+	*emit
+	*@param {string} eventName - name for events call
+	*@memberof ppEvents
+	*@returns {void}	    
+	*/
+	proto.emit = function( eventName ){
+		var i, listeners, length, args = [].slice.call(arguments, 1);
+		if (typeof events[eventName] === 'object') {
+		  listeners = events[eventName].slice();
+
+		  for (i = 0; i < listeners.length; i++) {
+		      listeners[i].apply(this, args);
+		  }
+
+		}
+	}
+	/**
+	*removeListener
+	*@param {string} eventName - name for events call
+	*@param {funcion} function - funcion will be remove 
+	*@memberof ppEvents
+	*@returns {void}	    
+	*/
+	proto.removeListener = function( eventName , callbacks ){
+			var idx;
+			if( typeof events[eventName] === 'object' ){
+				idx = events[eventName].indexOf(callbacks);
+				if( idx > -1 ){
+					events[eventName].splice(idx,1);
+				}
+			}
+	}
+  	return ppEvents;
+});	

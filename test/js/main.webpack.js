e161e8c8bbf97df4a0d9547c07d1fbee251866af
carlos-sweb <c4rl0sill3sc4@gmail.com>
2024-10-07 22:12:55 -0300

    fix error on removeListener

--- test/js/main.webpack.js
+++ test/js/main.webpack.js
@@ -1,14 +1,13 @@
+import ppEvents from './../../dist/pp-events.js';
+
+const Event = ppEvents()
+const Event2 = ppEvents()
 //------------------------------------------------------------------------------------
-var ppEvents = require("./pp-events.js")
-//------------------------------------------------------------------------------------
-var Event = ppEvents()
-var Event2 = ppEvents()
-//------------------------------------------------------------------------------------
-var sayHello = function( msg ){
+const sayHello = function( msg ){
 	console.log(msg)
 }
 //------------------------------------------------------------------------------------
-function A(msg1,msg2){
+const A = (msg1,msg2) => {
 	console.log("Title1: "+msg1)
 	console.log("Title2: "+msg2);
 }
@@ -27,6 +26,3 @@ Event2.on("sayHello", A )
 //------------------------------------------------------------------------------------
 Event.emit("sayHello","Custom Message","Other Custom Message" )
 //------------------------------------------------------------------------------------
-
-
-
